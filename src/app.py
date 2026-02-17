08fde738e32b4c3c57b47e0ee45ab0041cbb6c1f
Bryce Breedlove <brycebreedlo@gmail.com>
2026-02-16 20:46:52 -0600

    removed jinja

--- src/app.py
+++ src/app.py
@@ -19,12 +19,12 @@ app = FastAPI()
 # app.mount("../dist/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "../dist/static")), name="static")
 
 # Set up Jinja2 templates
-templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "../dist/templates"))
+# templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "../dist/templates"))
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     """Serve the portfolio homepage."""
-    return templates.TemplateResponse("index.html", {"request": request})
+    return await self.env.ASSETS.fetch(request)
 
 # if __name__ == "__main__":
 #     uvicorn.run("src.app:app", host="127.0.0.1", port=8000, reload=True)
